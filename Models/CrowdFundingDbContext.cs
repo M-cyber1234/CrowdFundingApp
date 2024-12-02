6170d9751274933986e3171f6eb55c28ea801a5e
Franck KN <fkinaninkaya@gmail.com>
2024-12-02 11:16:32 +0100

    Update CrowfundingContext.cs
    Just make the Dbset classes virtual

--- Models/CrowdFundingDbContext.cs
+++ Models/CrowdFundingDbContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CrowdFundingApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CrowdFundingApp.Models
 {
     public class CrowdFundingDbContext : DbContext
     {
         public CrowdFundingDbContext(DbContextOptions<CrowdFundingDbContext> options) : base(options) { }
-        DbSet<User> Users { get; set; }
+        public virtual DbSet<User> Users { get; set; }
         //DbSet<Project> Projects { get; set; }
-        DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
         //DbSet<Contribution> Contributions { get; set; }
+
     }
 }
