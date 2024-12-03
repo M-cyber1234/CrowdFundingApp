e4746c950d3566508ce7a403013934c8de7f0a20
Franck KN <fkinaninkaya@gmail.com>
2024-12-03 15:17:13 +0100

    Update User.cs
    Delete the Role Propety

--- Models/User.cs
+++ Models/User.cs
@@ -6,18 +6,16 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Role { get; set; } // "Creator" or "Contributor"
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; }
 
         public User() { }
-        public User(int userId, string name, string email, string password, string role, DateTime createdAt)
+        public User(int userId, string name, string email, string password, DateTime createdAt)
         {
             UserId = userId;
             Name = name;
             Email = email;
             Password = password;
-            Role = role;
             CreatedAt = createdAt;
         }
     }
